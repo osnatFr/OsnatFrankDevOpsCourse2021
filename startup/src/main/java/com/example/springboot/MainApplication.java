7936a9989fc7dae52274c6a13c4e6039fa33979d
אסנת פראנק <038090908@MBYdomain.local>
2021-11-01 11:35:48 +0200

    change main class name

--- startup/src/main/java/com/example/springboot/MainApplication.java
+++ startup/src/main/java/com/example/springboot/MainApplication.java
@@ -9,10 +9,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Application.class);
+		SpringApplication app = new SpringApplication(MainApplication.class);
         app.setDefaultProperties(Collections
           .singletonMap("server.port", "8083"));
         app.run(args);
